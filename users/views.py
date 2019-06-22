3b0edc6671dbb6270541e072e18ca116e151c32e
Jonathan Tran <43768831+jontran1@users.noreply.github.com>
2019-06-22 11:05:30 -0400

    The my stories page now displays stories belonging to the currently logged in user.

--- users/views.py
+++ users/views.py
@@ -36,8 +36,8 @@ def register(request):
 def my_stories(request):
     """Show all stories belonging to current logged in user."""
     stories = request.user.story_set.order_by('date_added')
-    context = {'stories' : stories}
-    return render(request, 'fan_fictions/stories.html', context)
+    context = {'stories': stories}
+    return render(request, 'fan_fictions/my_stories.html', context)
 
 @login_required
 def new_comment(request, story_id, entry_id):
